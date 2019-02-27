39d2da81af1c38ab41d7a1c65ba94bd11488b059
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2019-02-27 11:11:04 +0100

    Converting elements to polymer 3.0

--- frontend/components/tve-inline-equation.js
+++ frontend/components/tve-inline-equation.js
@@ -1,15 +1,10 @@
-/*<link rel="import" href="mathjax-include.html">*/
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-import '@polymer/polymer/polymer-legacy.js';
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 
 import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
-import './math-tex.js';
-Polymer({
-  _template: Polymer.html`
+
+class TVEInlineEquation extends PolymerElement {
+  static get template() {
+    return html`
 <style include="iron-flex"></style>
 
 <style>
@@ -19,14 +14,22 @@ Polymer({
 </style>
 
 <math-tex display="inline" content="{{content}}"></math-tex>
-`,
-
-  is: 'tve-inline-equation',
-
-  properties: {
-      content: {
-        	type: String,
-        	value: ""
-      }
+`;
   }
-});
+  static get properties() {
+   return {
+      content: {
+        type: String,
+        value: ""
+      }
+    }
+  }
+}
+
+customElements.define('tve-inline-equation', TVEInlineEquation);
+/*<link rel="import" href="mathjax-include.html">*/
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
