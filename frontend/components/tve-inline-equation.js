bcc7b2f7f3cd3d96a7b43eeb1fa5b78b541c8494
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2019-02-27 14:13:48 +0100

    Examining issues with the tve-inline-equation element.

--- frontend/components/tve-inline-equation.js
+++ frontend/components/tve-inline-equation.js
@@ -1,35 +1,13 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 
-import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
+// import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
 
 class TVEInlineEquation extends PolymerElement {
   static get template() {
     return html`
-<style include="iron-flex"></style>
-
-<style>
-:host {
-    display: inline;
-}
-</style>
-
-<math-tex display="inline" content="{{content}}"></math-tex>
+<math-tex display="inline"><slot>0</slot></math-tex>
 `;
-  }
-  static get properties() {
-   return {
-      content: {
-        type: String,
-        value: ""
-      }
-    }
   }
 }
 
 customElements.define('tve-inline-equation', TVEInlineEquation);
-/*<link rel="import" href="mathjax-include.html">*/
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
