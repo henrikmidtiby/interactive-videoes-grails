0d04d59539606c4858600e8554707dfc476bdec4
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2019-03-01 11:46:22 +0100

    Experimenting with katex-elements. It doesn't work right now however.

--- frontend/components/tve-inline-equation.js
+++ frontend/components/tve-inline-equation.js
@@ -1,12 +1,24 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import {KatexInline} from 'katex-elements/katex-inline.js';
 
 // import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
 
 class TVEInlineEquation extends PolymerElement {
   static get template() {
     return html`
-<math-tex display="inline"><slot>0</slot></math-tex>
+<katex-inline>{{content}}</katex-inline>
 `;
+  }
+  static get properties() {
+    return {
+      content: {
+        type: String, 
+        value: "", 
+      }
+    }
+  }
+  ready() {
+    super.ready();
   }
 }
 
