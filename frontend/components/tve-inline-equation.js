75e25e2dbaaf6e55792dd7ec198f64b5d4f9ec30
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2019-03-01 14:31:47 +0100

    Katex is working.

--- frontend/components/tve-inline-equation.js
+++ frontend/components/tve-inline-equation.js
@@ -1,12 +1,16 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import {KatexInline} from 'katex-elements/katex-inline.js';
+import {} from '@polymer/polymer/lib/utils/async.js';
 
 // import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
 
 class TVEInlineEquation extends PolymerElement {
   static get template() {
     return html`
-<katex-inline>{{content}}</katex-inline>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.css" integrity="sha384-b/NoaeRXkMxyKcrDw2KtVtYKkVg3dA0rTRgLoV7W2df3MzeR1eHLTi+l4//4fMwk" crossorigin="anonymous">
+<script defer src="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.js" integrity="sha384-ern5NCRqs6nJ/a4Ik0nB9hnKVH5HwV2XRUYdQl09OB/vvd1Lmmqbg1Mh+mYUclXx" crossorigin="anonymous"></script>
+
+<div id="mathcontent"></div>
 `;
   }
   static get properties() {
@@ -19,6 +23,19 @@ class TVEInlineEquation extends PolymerElement {
   }
   ready() {
     super.ready();
+    console.log("Setting timeout");
+//    setTimeout(this.insert_math_content.bind(this), 1000);
+    this.insert_math_content();
+    console.log("Finished setting timeout");
+  }
+  insert_math_content() {
+    console.log("<insert_math_content>");
+    var element = this.$.mathcontent;
+    console.log(this.content);
+    var temp = katex.renderToString(this.content, element, {displayMode: false, throwOnError: false});
+    console.log(temp);
+    element.innerHTML = temp;
+    console.log("</insert_math_content>");
   }
 }
 
