1b416574b71d2069c6eea31d576ef93b830fac94
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2019-03-02 15:10:55 +0100

    Added test cases for tve-exercise-eauations.

--- frontend/components/tv-exercise-equation.js
+++ frontend/components/tv-exercise-equation.js
@@ -5,6 +5,7 @@ class TVExerciseEquation extends PolymerElement {
   static get template() {
    return html`
 <style include="iron-flex"></style>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.css" integrity="sha384-b/NoaeRXkMxyKcrDw2KtVtYKkVg3dA0rTRgLoV7W2df3MzeR1eHLTi+l4//4fMwk" crossorigin="anonymous">
 
 <style>
 :host {
@@ -12,19 +13,29 @@ class TVExerciseEquation extends PolymerElement {
 }
 </style>
 
-<math-tex display="block" content="\\begin{align*}{{content}}\\end{align*}"></math-tex>
+<div id="mathcontent"></div>
+
 `;
   }
   static get properties() {
     return {
       content: {
         type: String, 
-        value: ""
+        value: "", 
+        observer: 'insert_math_content'
       }
     }
   }
-  constructor() {
-    super();
+  ready() {
+    super.ready();
+    this.insert_math_content();
+  }
+  insert_math_content() {
+    var element = this.$.mathcontent;
+    // var augmented_content = "\\begin{align*}" + this.content + "\\end{align*}";
+    // Katex does not support the align* environment.
+    var augmented_content = this.content;
+    katex.render(augmented_content, element, {displayMode: true, throwOnError: false});
   }
 }
 
